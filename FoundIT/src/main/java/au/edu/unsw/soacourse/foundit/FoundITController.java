fd857b2fb55aebf01a88b42a6df0534d18a85166
Tu-Trung Richie Lam <trung_lam@live.com.au>
2017-05-09 00:58:05 +1000

    Setup for client

---
+++ FoundIT/src/main/java/au/edu/unsw/soacourse/foundit/FoundITController.java
@@ -0,0 +1,20 @@
+package au.edu.unsw.soacourse.foundit;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class FoundITController {
+	
+	@RequestMapping("/")
+	public ModelAndView indexAction() {
+		return new ModelAndView("index");
+	}
+	
+	@RequestMapping(value = "/demo", method = RequestMethod.GET)
+	public ModelAndView demoAction() {
+		return new ModelAndView("index", "link", "http://www.baeldung.com/spring-requestmapping");
+	}
+}
