8952edb18352a5a01bc6d209643d8c18bc862553
Tu-Trung Richie Lam <trung_lam@live.com.au>
2017-05-10 02:47:55 +1000

    changed configs

--- FoundIT/src/main/java/au/edu/unsw/soacourse/foundit/controller/FoundITController.java
+++ FoundIT/src/main/java/au/edu/unsw/soacourse/foundit/controller/FoundITController.java
@@ -1,4 +1,4 @@
-package au.edu.unsw.soacourse.foundit;
+package au.edu.unsw.soacourse.foundit.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
